357dfed8c956b3cb8cca65ffd97695f416e6b703
Devwarlt <nadio.pontes@redes.unb.br>
2021-05-08 15:36:17 -0300

    Teste para cifra de César ROT-13

---
+++ src/test_rot13_encryption.py
@@ -0,0 +1,14 @@
+from encryption.rot13 import Rot13
+from sys import exit
+
+
+plain_text: str = input("Insert a plain-text value for Rot13 encryption: ")
+print(f"Plain-text: {plain_text}")
+
+rot13_encrypted_text: str = Rot13.encrypt(plain_text)
+print(f"Encrypted text: {rot13_encrypted_text}")
+
+rot13_decrypted_text: str = Rot13.decrypt(rot13_encrypted_text)
+print(f"Decrypted text: {rot13_decrypted_text}")
+
+exit(0)
